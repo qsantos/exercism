f16e5529e2be3e96649c6120a07f7ac7dfcd1281
Quentin Santos <qsantos@qsantos.fr>
2023-01-13 20:17:10 +0100

    Add luhn

---
+++ rust/luhn/src/lib.rs
@@ -0,0 +1,25 @@
+/// Check a Luhn checksum.
+pub fn is_valid(code: &str) -> bool {
+    let mut double = false;
+    let mut s = 0;
+    let mut digits = 0;
+    for &c in code.as_bytes().iter().rev() {
+        if c == b' ' {
+            continue;
+        }
+        if !(b'0'..=b'9').contains(&c) {
+            return false;
+        }
+        let mut v = c - b'0';
+        if double {
+            v *= 2;
+            if v >= 10 {
+                v -= 9;
+            }
+        }
+        s += v;
+        double = !double;
+        digits += 1;
+    }
+    digits >= 2 && s % 10 == 0
+}
