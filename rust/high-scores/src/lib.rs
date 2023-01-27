8c3f825f81bd575690c932a83b6c4149991b8de9
Quentin Santos <qsantos@qsantos.fr>
2023-01-27 08:23:05 +0100

    Add high-scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,30 @@
+#![feature(binary_heap_drain_sorted)]
+use std::collections::BinaryHeap;
+
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut heap: BinaryHeap<u32> = self.scores.iter().copied().collect();
+        heap.drain_sorted().take(3).collect()
+    }
+}
