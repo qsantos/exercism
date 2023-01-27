073139f90a1267304475242397e59b6de91d0a94
Quentin Santos <qsantos@qsantos.fr>
2023-01-27 07:03:55 +0100

    Add beer-song

---
+++ rust/beer-song/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn verse(n: u32) -> String {
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
+        2 => String::from("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"),
+        n => format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n - 1),
+    }
+}
+
+pub fn sing(start: u32, end: u32) -> String {
+    if start < end {
+        (start..=end).map(verse).collect::<Vec<_>>()
+    } else {
+        (end..=start).rev().map(verse).collect::<Vec<_>>()
+    }
+    .join("\n")
+}
