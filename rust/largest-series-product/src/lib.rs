4d942a4bdcb21da3f1e75337cb67292a8efb8c9e
Quentin Santos <qsantos@qsantos.fr>
2023-02-22 09:53:18 +0100

    Add largest-series-product

---
+++ rust/largest-series-product/src/lib.rs
@@ -0,0 +1,24 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+    SpanTooLong,
+    InvalidDigit(char),
+}
+
+pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
+    if span == 0 {
+        return Ok(1);
+    } else if span > string_digits.len() {
+        return Err(Error::SpanTooLong);
+    }
+    for c in string_digits.chars() {
+        if !('0'..='9').contains(&c) {
+            return Err(Error::InvalidDigit(c));
+        }
+    }
+    Ok(string_digits
+        .as_bytes()
+        .windows(span)
+        .map(|chunk| chunk.iter().map(|digit| (digit - b'0') as u64).product())
+        .max()
+        .unwrap())
+}
