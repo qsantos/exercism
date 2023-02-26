6021a7c14f1ed7e37e11aff891cd03f7bda8f178
Quentin Santos <qsantos@qsantos.fr>
2023-02-26 12:31:52 +0100

    Add luhn-from

---
+++ rust/luhn-from/src/lib.rs
@@ -0,0 +1,40 @@
+use std::fmt::Display;
+
+pub struct Luhn {
+    code: String,
+}
+
+impl Luhn {
+    pub fn is_valid(&self) -> bool {
+        let mut double = false;
+        let mut s = 0;
+        let mut digits = 0;
+        for &c in self.code.as_bytes().iter().rev() {
+            if c == b' ' {
+                continue;
+            }
+            if !(b'0'..=b'9').contains(&c) {
+                return false;
+            }
+            let mut v = c - b'0';
+            if double {
+                v *= 2;
+                if v >= 10 {
+                    v -= 9;
+                }
+            }
+            s += v;
+            double = !double;
+            digits += 1;
+        }
+        digits >= 2 && s % 10 == 0
+    }
+}
+
+impl<T: Display> From<T> for Luhn {
+    fn from(input: T) -> Self {
+        Luhn {
+            code: format!("{}", input),
+        }
+    }
+}
